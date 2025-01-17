f5283eab8d1ae7cb406c8230732db37e83d3ce31
jeeva07-tech <jj1082202@gmail.com>
2025-01-17 14:39:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/welcome/OneDrive/Desktop/project/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/welcome/OneDrive/Desktop/project/heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open( 'C:/Users/welcome/OneDrive/Desktop/project/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open( 'parkinsons_model.sav', 'rb'))
 
 
 
